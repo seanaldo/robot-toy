5467e2f92b926afdfdaa7839e3d072c7a6b3d01f
seanaldo <sean.liew@smsmt.com>
2016-04-06 15:12:36 +1000

    finished game

--- src/app/controllers/robot.controller.js
+++ src/app/controllers/robot.controller.js
@@ -12,7 +12,7 @@
       vm.isGreat == false;
       //initialise global poker balance
 
-      vm.message = null; vm.robotPosition = null; vm.direction = 'west'; 
+      vm.message = null; vm.robotPosition = null; vm.direction = 'north'; 
 
       vm.table = [];
 
@@ -38,46 +38,74 @@
             cell.filling = false;
             row.push(cell);
           }
-          vm.table.push(row);
+          vm.table.unshift(row);
         }
       }
 
       $scope.place = function(cell) {
-        cell.filling=true;
-        vm.robotPosition = cell.position;
+        if (!vm.robotPosition) {
+          cell.filling=true;
+          vm.robotPosition = cell.position;
+        }
       }
 
       $scope.move = function() {
-
+        var tableLocation = vm.table[vm.robotPosition[0]][vm.robotPosition[1]].position;
+        var row = tableLocation[0];
+        var column = tableLocation[1];
+        vm.table[row][column].filling = false;
+        switch(vm.direction) {
+          case 'north':
+            row--;
+            break;
+          case 'west':
+            column--;
+            break;
+          case 'east':
+            column++;
+            break;
+          case 'south':
+            row++;
+            break;
+          default: break;
+        }
+        moveRobot(row,column);
       }
 
-      $scope.left = function() {
-        
-      }
-
-      $scope.right = function() {
-        
+      function moveRobot(row,column) {
+        vm.table[row][column].filling = true;
+        vm.robotPosition = vm.table[row][column].position;
       }
 
       $scope.report = function() {
-        
+        vm.message = "Robot is sitting at "+vm.robotPosition+" facing "+vm.direction;
       }
 
-      $scope.rotate = function(direction) {
-        switch (vm.direction) {
-          case 'north':
-            vm.direction = (direction == 'left') ? 'west' : 'east';
-            break;
-          case 'west':
-            vm.direction = (direction == 'left') ? 'south' : 'north';
-            break;
-          case 'east':
-            vm.direction = (direction == 'left') ? 'north' : 'south';
-            break;
-          case 'south':
-            vm.direction = (direction == 'left') ? 'east' : 'west';
-            break;
-          default: return;
+      $scope.rotate = function(isLeft) {
+        if (vm.direction == 'north') {
+          vm.direction = (isLeft) ? 'west' : 'east';
+        } else if (vm.direction == 'west') {
+          vm.direction = (isLeft) ? 'south' : 'north';
+        } else if (vm.direction == 'east') {
+          vm.direction = (isLeft) ? 'north' : 'south';
+        } else if (vm.direction == 'south') {
+          vm.direction = (isLeft) ? 'east' : 'west';
+        }
+      }
+
+      $scope.cannotMove = function(direction) {
+        if (vm.robotPosition != null) {
+          var verdict = true;
+          if (direction == 'north') {
+            verdict = (vm.robotPosition[0] == 4) ? true : false;
+          } else if (direction == 'west') {
+            verdict = (vm.robotPosition[1] == 0) ? true : false;
+          } else if (direction == 'east') {
+            verdict = (vm.robotPosition[1] == 4) ? true : false;
+          } else if (direction == 'south') {
+            verdict = (vm.robotPosition[0] == 0) ? true : false;
+          }
+          return verdict;
         }
       }
 
